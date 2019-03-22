a1986d5fc2006d50af09857cf916a081552c3924
Nick Bansal <extreme_wired@msn.com>
2019-03-22 11:51:58 +0000

    Added Image

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import LakeImage from "../Images/Lake.jpg";
 
 const Main = styled.div`
   text-align: center;
   font-family: 'Oxygen', sans-serif;
 `;
 
+const Image = styled.img`
+  width: 100%;
+`;
+
 const App = () => (
   <Main>
     <h1>Hello</h1>
+    <Image src={LakeImage} alt="Lake" />
   </Main>
 );
 
