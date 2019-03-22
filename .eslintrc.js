7e5238183a9fa58393e1b38220a9fbd9cfceddcb
Nick Bansal <extreme_wired@msn.com>
2019-03-22 11:32:00 +0000

    My first commit

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "mocha": true
+    },
+    "extends": "airbnb",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [
+            1,
+            {
+                "extensions": [
+                    ".js",
+                    ".jsx"
+                ]
+            }
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  text-align: center;
+  font-family: 'Oxygen', sans-serif;
+`;
+
+const App = () => (
+  <Main>
+    <h1>Hello</h1>
+  </Main>
+);
+
+
+export default App;
