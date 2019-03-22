1229c04dd926a4762ebd94e063fe5c4666aff852
Nick Bansal <extreme_wired@msn.com>
2019-03-22 16:35:09 +0000

    parallax working

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,16 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import styled from "styled-components";
-import LakeImage from "../Images/Lake.jpg";
-
-const Main = styled.div`
-  text-align: center;
-  font-family: 'Oxygen', sans-serif;
-  position: relative
-`;
+// import MainDiv from "Theme";
+import Plx from 'react-plx';
+import Forest from "Images/Forest.jpg";
 
 const Title = styled.h1`
+  color: white;
   position: absolute;
-  left: 50%;
-  top: 50px;
-  transform: translate(-50%, -50%)
+  text-shadow: 2px 2px 2px black;
+  font-size: 45px
 `;
-
-const Image = styled.img`
-  width: 100%;
-  `;
 
 const Para = styled.p`
   position: absolute;
@@ -28,30 +20,67 @@ const Para = styled.p`
   padding: 50px;
   background: whitesmoke;
   margin: 0;
-  font-size: 17px;
+  font-size: 20px;
   opacity: 0.7;
 `;
 
+const style = {
+  textAlign: 'center',
+  fontFamily: 'Oxygen, sans-serif',
+  position: 'relative',
+  backgroundImage: `url(${Forest})`,
+  backgroundPosition: 'center',
+  width: '100%',
+  height: '100vh',
+  paddingTop: '20px',
+
+
+  display: 'flex',
+  justifyContent: 'center'
+}
+
+const parallaxData = [
+  {
+    start: '60px',
+    end: '500px',
+    properties: [
+      {
+        startValue: 100,
+        endValue: 0,
+        property: 'height',
+        unit: 'vh'
+      },
+    ],
+  },
+];
+
 const App = () => (
-  <Main>
-    <Title>Main Page</Title>
-    <Image src={LakeImage} alt="Lake" />
+  <div style={{ height: '3000px' }}>
+
+    <Plx
+      style={style}
+      parallaxData={parallaxData}
+    >
+      {/* <MainDiv> */}
+      <Title>Parallax</Title>
+      {/* </MainDiv> */}
+    </Plx>
     <Para>
       {`Contrary to popular belief, Lorem Ipsum is not simply random text.
-      It has roots in a piece of classical Latin literature from 45 BC, 
-      making it over 2000 years old.
-      Richard McClintock, a Latin professor at 
-      Hampden-Sydney College in Virginia, looked up one of the more 
-      obscure Latin words, consectetur, from a Lorem Ipsum passage, 
-      and going through the cites of the word in classical literature, 
-      discovered the undoubtable source. Lorem Ipsum comes from sections 
-      1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" 
-      (The Extremes of Good and Evil) by Cicero, written in 45 BC. 
-      This book is a treatise on the theory of ethics, 
-      very popular during the Renaissance. The first line of Lorem Ipsum, 
-      "Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32.`}
+        It has roots in a piece of classical Latin literature from 45 BC, 
+        making it over 2000 years old.
+        Richard McClintock, a Latin professor at 
+        Hampden-Sydney College in Virginia, looked up one of the more 
+        obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+        and going through the cites of the word in classical literature, 
+        discovered the undoubtable source. Lorem Ipsum comes from sections 
+        1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" 
+        (The Extremes of Good and Evil) by Cicero, written in 45 BC. 
+        This book is a treatise on the theory of ethics, 
+        very popular during the Renaissance. The first line of Lorem Ipsum, 
+        "Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32.`}
     </Para>
-  </Main>
+  </div>
 );
 
 
--- src/Theme/index.js
+++ src/Theme/index.js
@@ -1,4 +1,5 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+import Forest from "Images/Forest.jpg";
 
 /* eslint-disable */
 
@@ -9,3 +10,21 @@ createGlobalStyle`
         font-family: 'Oxygen', sans-serif
     }
 `;
+
+
+const MainDiv = styled.div`
+  text-align: center;
+  font-family: 'Oxygen', sans-serif;
+  position: relative;
+  background-image: url(${Forest});
+  background-position: center;
+  width: 100%;
+  height: 100vh;
+  padding-top: 20px
+
+
+  display: flex;
+  justify-content: center;
+`;
+
+export default MainDiv
